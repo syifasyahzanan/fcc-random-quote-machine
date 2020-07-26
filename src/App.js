b19d413d5c281c0c9466696530b77a3e44752d53
Syifa Syahzanan Sudradjat <syifasyahzanan@iMac-Syifa.local>
2020-07-26 14:08:18 +0700

    Clean Code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { random } from 'lodash';
+import 'typeface-roboto';
+import Grid from '@material-ui/core/Grid';
+import {withStyles} from '@material-ui/core'
+import HadithMachine from './components/HadithMachine';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = {
+  container: {
+    display: 'flex',
+    height: '100vh',
+    alignItems: 'center'
+  }
 }
 
-export default App;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hadith: [],
+      selectedHadithIndex: null
+    }
+    this.assignNewHadithIndex = this.assignNewHadithIndex.bind(this)
+    this.generateNewHadithIndex = this.generateNewHadithIndex.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://gist.githubusercontent.com/syifasyahzanan/0d4da0bc11bb6b052f0d56f270070c33/raw/18c26594deefe281e2ac6b2c39caf597d341c67f/hadith.json')
+      .then(data => data.json())
+      .then(quotes => this.setState({ hadith: quotes }, this.assignNewHadithIndex));
+  }
+
+  get generateNewHadith() {
+    if (!this.state.hadith.length || !Number.isInteger(this.state.selectedHadithIndex)) {
+      return undefined;
+    }
+    return this.state.hadith[this.state.selectedHadithIndex];
+  }
+
+  generateNewHadithIndex() {
+    if (!this.state.hadith.length) {
+      return undefined;
+    }
+    return random(0, this.state.hadith.length - 1);
+  }
+
+  assignNewHadithIndex () {
+    this.setState({ selectedHadithIndex: this.generateNewHadithIndex() })
+  }
+
+
+  render() {
+    return (
+      <Grid className={this.props.classes.container} id="quote-box" justify="center" container>
+        <Grid xs={11} lg={4} item>
+          {
+            this.generateNewHadith
+            ? <HadithMachine generateNewHadith={this.generateNewHadith} assignNewHadithIndex={this.assignNewHadithIndex} />
+            : null
+          }
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+export default withStyles(styles)(App);
